7c80505c73289c56b232bf56c7242fcc27244e45
Dmen01 <dyperfect60@gmail.com>
2026-06-19 18:57:10 +0530

    Initial commit for deployment

--- server/controllers/phdController.js
+++ server/controllers/phdController.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-// ── Multer Configuration ──────────────────────────────────────────────────────
+// Multer Configuration
 const storagePresentation = multer.diskStorage({
     destination: (req, file, cb) => {
         const dir = 'uploads/phd_presentations';
